4dbee2865e7243e5174c5187df81e0ced9036b2f
Omollo <KEN20969@kcbgroup.com>
2022-08-13 11:38:29 +0300

    initial commit

---
+++ IN PROGRESS/kcb-spring-security/src/main/java/com/kcbgroup/main/services/MyUserDetailsClass.java
@@ -0,0 +1,29 @@
+package com.kcbgroup.main.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.kcbgroup.main.models.MyUserDetails;
+import com.kcbgroup.main.models.User;
+import com.kcbgroup.main.repository.UserRepository;
+
+
+public class MyUserDetailsClass implements UserDetailsService{
+	
+	@Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        return user.map(MyUserDetails::new).get();
+
+}
+}
